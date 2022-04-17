b4505197e59d514fe0457428aafaac25224e72c9
aleksei_fedorov@epam.com <aleksei_fedorov@epam.com>
2022-04-17 16:54:46 +0300

    Slightly reformatted

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
 import subprocess
 
+
 class HttpGetHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print("GET "+ self.path)
+        print("GET " + self.path)
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         result = subprocess.run(['tldr', self.path], stdout=subprocess.PIPE)
         self.wfile.write(result.stdout)
         self.wfile.flush()
+
 
 def run(server_class=HTTPServer, handler_class=HttpGetHandler):
     server_address = ('', 8000)
@@ -20,5 +22,6 @@ def run(server_class=HTTPServer, handler_class=HttpGetHandler):
         httpd.serve_forever()
     except KeyboardInterrupt:
         httpd.server_close()
+
 
 run()
